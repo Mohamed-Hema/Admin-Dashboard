3920582e3f305d11b3c90b85f40bd90fcb29d95a
Mohamed Ibrahim <mohamedhema2017@gmail.com>
2024-11-22 23:57:18 +0200

    last update

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "state";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
