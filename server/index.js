9fece1dcf277aa6943ef6bfdd5d2cd75ac46add1
Mohamed Ibrahim <mohamedhema2017@gmail.com>
2024-11-22 23:39:20 +0200

    base is done

--- server/index.js
+++ server/index.js
@@ -6,9 +6,9 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import clientRoutes from "./routes/client.js";
-import generalRoutes from "./routes/generalRoutes";
-import managementRoutes from "./routes/generalRoutes";
-import salesRoutes from "./routes/generalRoutes";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 // Configuration
 dotenv.config();
@@ -26,3 +26,12 @@ app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+
+// Mongoose Setup
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
